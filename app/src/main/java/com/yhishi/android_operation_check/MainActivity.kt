20efada96354bf6258e880e330038706bcd0de05
yhishi <ev422119.s.okayama@gmail.com>
2022-04-27 14:42:53 +0900

    通常のSharedPreferencesのお試し実装

--- app/src/main/java/com/yhishi/android_operation_check/MainActivity.kt
+++ app/src/main/java/com/yhishi/android_operation_check/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.yhishi.android_operation_check
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // SharedPreferences
+        val normalPreferences = getSharedPreferences(NORMAL_FILE_NAME, Context.MODE_PRIVATE)
+
+        normalPreferences
+            .edit()
+            .putString("normal-preferences-key", "normal-preferences-value")
+            .apply()
+
+        val normalPreferencesValue = normalPreferences.getString("normal-preferences-key", "Nothing")
+        Toast.makeText(this, "normalPreferencesValue = $normalPreferencesValue", Toast.LENGTH_LONG).show()
+    }
+
+    companion object {
+        private const val NORMAL_FILE_NAME = "normal-preferences"
     }
 }
