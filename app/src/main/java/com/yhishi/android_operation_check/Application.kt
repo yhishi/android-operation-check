aab30f425e52e2f84b782c6a2692392ef0aed242
yhishi <ev422119.s.okayama@gmail.com>
2022-04-27 15:52:14 +0900

    Hilt初期設定

---
+++ app/src/main/java/com/yhishi/android_operation_check/Application.kt
@@ -0,0 +1,11 @@
+package com.yhishi.android_operation_check
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class Application : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
