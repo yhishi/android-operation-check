3ea602944e5d8fb656e6daf0fcc41666e24d324a
yhishi <ev422119.s.okayama@gmail.com>
2022-04-27 14:44:07 +0900

    EncryptedSharedPreferencesのお試し実装

--- app/src/main/java/com/yhishi/android_operation_check/MainActivity.kt
+++ app/src/main/java/com/yhishi/android_operation_check/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKeys
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,9 +22,28 @@ class MainActivity : AppCompatActivity() {
 
         val normalPreferencesValue = normalPreferences.getString("normal-preferences-key", "Nothing")
         Toast.makeText(this, "normalPreferencesValue = $normalPreferencesValue", Toast.LENGTH_LONG).show()
+
+        // EncryptedSharedPreferences
+        val mainKeyAlias = MasterKeys.getOrCreate(MasterKeys.AES256_GCM_SPEC)
+        val encryptedPreferences = EncryptedSharedPreferences.create(
+            ENCRYPTED_FILE_NAME,
+            mainKeyAlias,
+            applicationContext,
+            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+        )
+
+        encryptedPreferences
+            .edit()
+            .putString("encrypted-preferences-key", "encrypted-preferences-value")
+            .apply()
+
+        val encryptedPreferencesValue = encryptedPreferences.getString("encrypted-preferences-key", "Nothing")
+        Toast.makeText(this, "encryptedPreferencesValue = $encryptedPreferencesValue", Toast.LENGTH_LONG).show()
     }
 
     companion object {
         private const val NORMAL_FILE_NAME = "normal-preferences"
+        private const val ENCRYPTED_FILE_NAME = "encrypted-preferences"
     }
 }
