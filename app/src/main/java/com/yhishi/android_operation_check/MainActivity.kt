5f07e741cbd2328dededbfae10cf978334477a13
yhishi <ev422119.s.okayama@gmail.com>
2022-05-04 18:10:26 +0900

    Preferencesの処理をMainFragmentに移動

--- app/src/main/java/com/yhishi/android_operation_check/MainActivity.kt
+++ app/src/main/java/com/yhishi/android_operation_check/MainActivity.kt
@@ -1,52 +1,15 @@
 package com.yhishi.android_operation_check
 
 import android.os.Bundle
-import android.widget.Toast
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.lifecycleScope
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.flow.filter
-import kotlinx.coroutines.launch
 
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    private val viewModel: MainViewModel by viewModels()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        lifecycleScope.launch {
-            viewModel.normalPreferencesValue
-                .filter {
-                    it.isNotEmpty()
-                }
-                .collect { normalPreferencesValue ->
-                    Toast.makeText(this@MainActivity, "normalPreferencesValue = $normalPreferencesValue", Toast.LENGTH_LONG).show()
-                }
-        }
-
-        lifecycleScope.launch {
-            viewModel.encryptedPreferencesValue
-                .filter {
-                    it.isNotEmpty()
-                }
-                .collect { encryptedPreferencesValue ->
-                    Toast.makeText(this@MainActivity, "encryptedPreferencesValue = $encryptedPreferencesValue", Toast.LENGTH_LONG).show()
-                }
-        }
-
-        lifecycleScope.launch {
-            viewModel.preferencesDataStoreValue
-                .filter {
-                    it.isNotEmpty()
-                }
-                .collect { preferencesDataStoreValue ->
-                    Toast.makeText(this@MainActivity, "preferencesDataStoreValue = $preferencesDataStoreValue", Toast.LENGTH_LONG).show()
-                }
-        }
     }
 }
--- app/src/main/java/com/yhishi/android_operation_check/MainFragment.kt
+++ app/src/main/java/com/yhishi/android_operation_check/MainFragment.kt
@@ -1,16 +1,23 @@
 package com.yhishi.android_operation_check
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.yhishi.android_operation_check.databinding.FragmentMainBinding
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.launch
 
 
+@AndroidEntryPoint
 class MainFragment : Fragment() {
-
     private lateinit var binding: FragmentMainBinding
+    private val viewModel: MainViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,5 +25,39 @@ class MainFragment : Fragment() {
     ): View? {
         binding = FragmentMainBinding.inflate(inflater, container, false)
         return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        lifecycleScope.launch {
+            viewModel.normalPreferencesValue
+                .filter {
+                    it.isNotEmpty()
+                }
+                .collect { normalPreferencesValue ->
+                    Toast.makeText(requireContext(), "normalPreferencesValue = $normalPreferencesValue", Toast.LENGTH_LONG).show()
+                }
+        }
+
+        lifecycleScope.launch {
+            viewModel.encryptedPreferencesValue
+                .filter {
+                    it.isNotEmpty()
+                }
+                .collect { encryptedPreferencesValue ->
+                    Toast.makeText(requireContext(), "encryptedPreferencesValue = $encryptedPreferencesValue", Toast.LENGTH_LONG).show()
+                }
+        }
+
+        lifecycleScope.launch {
+            viewModel.preferencesDataStoreValue
+                .filter {
+                    it.isNotEmpty()
+                }
+                .collect { preferencesDataStoreValue ->
+                    Toast.makeText(requireContext(), "preferencesDataStoreValue = $preferencesDataStoreValue", Toast.LENGTH_LONG).show()
+                }
+        }
     }
 }
