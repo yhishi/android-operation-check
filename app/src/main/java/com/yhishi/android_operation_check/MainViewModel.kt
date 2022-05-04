fa64254202e7bd7c54df2d8b002459ea4d9c070c
yhishi <ev422119.s.okayama@gmail.com>
2022-05-04 18:51:01 +0900

    ハッシュ計算処理

--- app/src/main/java/com/yhishi/android_operation_check/MainViewModel.kt
+++ app/src/main/java/com/yhishi/android_operation_check/MainViewModel.kt
@@ -6,7 +6,9 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
+import java.security.MessageDigest
 import javax.inject.Inject
+import kotlin.text.Charsets.UTF_8
 
 
 @HiltViewModel
@@ -42,5 +44,23 @@ class MainViewModel @Inject constructor(
                     _preferencesDataStoreValue.value = it
                 }
         }
+    }
+
+    fun onClickHashButton(input: String) {
+        val toMd5 = calculateHash(input = input, algorithm = ALGORITHM_MD5)
+        val toSha = calculateHash(input = input, algorithm = ALGORITHM_SHA_256)
+    }
+
+    private fun calculateHash(input: String, algorithm: String): String {
+        // MD5ハッシュ計算
+        val byteArray = MessageDigest.getInstance(algorithm).digest(input.toByteArray(UTF_8))
+
+        // バイト配列を16進数の文字列に変換
+        return byteArray.joinToString(separator = "") { byte -> "%02x".format(byte) }
+    }
+
+    companion object {
+        private const val ALGORITHM_MD5 = "MD5"
+        private const val ALGORITHM_SHA_256 = "SHA-256"
     }
 }
