a0d62ce8eaf11c56f3748408e3f0b210ab3772ea
yhishi <ev422119.s.okayama@gmail.com>
2022-06-22 09:42:14 +0900

    カメラ権限のリクエスト処理

--- app/src/main/java/com/yhishi/android_operation_check/CameraImageGalleryActivity.kt
+++ app/src/main/java/com/yhishi/android_operation_check/CameraImageGalleryActivity.kt
@@ -1,6 +1,9 @@
 package com.yhishi.android_operation_check
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -8,8 +11,23 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class CameraImageGalleryActivity : AppCompatActivity() {
 
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
+            if (isGranted) {
+                // takePicture()
+            } else {
+                // TODO
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_camera_image_gallery)
+
+        if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            // takePicture()
+        } else {
+            requestPermissionLauncher.launch(Manifest.permission.CAMERA)
+        }
     }
 }
